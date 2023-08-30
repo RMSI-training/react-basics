6320de7fb47aace0431648ed4be4e2aed3fd2748
Pariwesh Gupta (C) <pariwesh@alchemysolutions.net>
2023-08-30 22:44:23 +0530

    ajax request

--- src/Userform.js
+++ src/Userform.js
@@ -7,13 +7,31 @@ function Userform() { //functional component
     lastname: 'Carter'
   });
   const updateValue = (event) => {
-    setUser({...user, [event.target.name]: event.target.value });
+    setUser({ ...user, [event.target.name]: event.target.value });
+  }
+  function save() {
+    const promise = fetch("http://localhost:3001/users", {//ajax
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(user)
+    });
+    promise.then(function (response) {//100-399
+      console.log(response);
+    });
+    promise.catch(function (error) {//400-599
+      console.log(error);
+    })
+    console.log(user);
   }
   //logic 
   return (//jsx
-    <span> //view
+    <span>
       <input value={user.firstname} name='firstname' onChange={updateValue}></input>
       <input value={user.lastname} name='lastname' onChange={updateValue}></input>
+      <input type='radio' name="gender" value='Male' onChange={updateValue} />Male
+      <input type='radio' name="gender" value='Female' onChange={updateValue} />Female
+
+      <button onClick={save}>Save</button>
     </span>
   )
 }
