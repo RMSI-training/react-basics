a994b644b495d6b8746fd936fea9174eb6f8dfbe
Pariwesh Gupta (C) <pariwesh@alchemysolutions.net>
2023-09-15 21:01:19 +0530

    added pages

--- src/Login.js
+++ src/Login.js
@@ -3,26 +3,26 @@ import { toast, ToastContainer } from "react-toastify";
 
 export const Login = () => {
     const [credentials, setCredentials] = useState({
-        login:'',
+        login: '',
         pwd: ''
     });
     function login(e) {
         e.preventDefault();
-        if(credentials.login.trim() === '' ){
-            toast.warning('Please enter user name',{
+        if (credentials.login.trim() === '') {
+            toast.warning('Please enter user name', {
                 closeOnClick: true
             });
             return false;
         }
-        if(credentials.pwd.trim() === '' ){
+        if (credentials.pwd.trim() === '') {
             toast.warning('Please enter password');
             return false;
         }
-        
+
     }
     const updateValue = (event) => {
         setCredentials({ ...credentials, [event.target.name]: event.target.value });
-      }
+    }
     return (<div className="row">
         <div className="offset-lg-3 col-lg-6">
             <form onSubmit={login} className="container">
@@ -31,7 +31,7 @@ export const Login = () => {
                         <input name='login' onChange={updateValue} value={credentials.login} placeholder="User name" className="form-control"></input>
                     </div>
                     <div className="form-group">
-                        <input value={credentials.pwd} placeholder="Password" className="form-control"></input>
+                        <input name='pwd' value={credentials.pwd} placeholder="Password" className="form-control"></input>
                     </div>
                 </div>
                 <button type="submit" className="btn btn-success" >Login</button>
--- src/Register.js
+++ src/Register.js
@@ -1,31 +1,41 @@
 import { useState } from "react";
 
 function Register() {
-    const [registerData,setRegisterData] = useState({
-        firstName:'',
-        lastname:'',
-        email:'',
-        pwd:''
+    const [registerData, setRegisterData] = useState({
+        firstName: 'Pariwesh',
+        lastname: '',
+        email: '',
+        pwd: ''
     });
     const updateValue = (event) => {
         setRegisterData({ ...registerData, [event.target.name]: event.target.value });
-      }
+    }
+    function reset() {
+        setRegisterData({ firstName: '',
+        lastname: '',
+        email: '',
+        pwd: ''});
+    }
     return (
         <div>
             <form className="container">
                 <div className="card-body">
                     <div className="form-group">
-                        <input name='login' onChange={updateValue} value={registerData.firstName} placeholder="User name" className="form-control"></input>
+                        <input name='firstName' onChange={updateValue} value={registerData.firstName} placeholder="User name" className="form-control" ></input>
                     </div>
                     <div className="form-group">
-                        <input value={registerData.lastname} placeholder="Password" className="form-control"></input>
+                        <input value={registerData.lastname} placeholder="Last Name" className="form-control" name="lastname" onChange={updateValue}></input>
                     </div>
                     <div className="form-group">
-                        <input name='login' onChange={updateValue} value={registerData.email} placeholder="User name" className="form-control"></input>
+                        <input value={registerData.pwd} placeholder="Password" className="form-control" name="pwd" onChange={updateValue} type='password'></input>
                     </div>
                     <div className="form-group">
-                        <input value={registerData.pwd} placeholder="Password" className="form-control"></input>
+                        <input name='email' onChange={updateValue} value={registerData.email} placeholder="Email" className="form-control"></input>
                     </div>
+                    
+                    <button type="submit" className="btn btn-success" >Register</button>
+                    <button type="button" className="btn btn-success" onClick={reset}>Reset</button>
+
                 </div>
             </form>
         </div>
