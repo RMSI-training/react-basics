0535e10fae13ce495a1f64ef59099b73d879a6b6
Pariwesh Gupta (C) <pariwesh@alchemysolutions.net>
2023-09-19 21:50:08 +0530

    handle login success

--- src/Login.js
+++ src/Login.js
@@ -1,24 +1,31 @@
+import axios from "axios";
 import { useState } from "react";
 import { toast, ToastContainer } from "react-toastify";
 
 export const Login = () => {
     const [credentials, setCredentials] = useState({
-        login: '',
-        pwd: ''
+        email: '',
+        password: ''
     });
-    function login(e) {
+    async function login(e) {
         e.preventDefault();
-        if (credentials.login.trim() === '') {
+        if (credentials.email.trim() === '') {
             toast.warning('Please enter user name', {
                 closeOnClick: true
             });
             return false;
         }
-        if (credentials.pwd.trim() === '') {
+        if (credentials.password.trim() === '') {
             toast.warning('Please enter password');
             return false;
         }
-
+        const response  = await axios.post('http://localhost:3001/login',credentials);
+        if (response.status ==200) {
+            toast.success("login successful");
+            sessionStorage.setItem('token', response.data.accessToken);
+        } else {
+            toast.error("login failed");
+        }
     }
     const updateValue = (event) => {
         setCredentials({ ...credentials, [event.target.name]: event.target.value });
@@ -28,10 +35,10 @@ export const Login = () => {
             <form onSubmit={login} className="container">
                 <div className="card-body">
                     <div className="form-group">
-                        <input name='login' onChange={updateValue} value={credentials.login} placeholder="User name" className="form-control"></input>
+                        <input name='email' onChange={updateValue} value={credentials.email} placeholder="Email" className="form-control"></input>
                     </div>
                     <div className="form-group">
-                        <input name='pwd' value={credentials.pwd} placeholder="Password" className="form-control"></input>
+                        <input name='password' onChange={updateValue} value={credentials.password} placeholder="Password" className="form-control"></input>
                     </div>
                 </div>
                 <button type="submit" className="btn btn-success" onClick={login}>Login</button>
